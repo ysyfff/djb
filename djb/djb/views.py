11633b4ca3ae1d530e9e26ed5a5c8abe0b0c5544
ysyong <1156546473@qq.com>
2013-05-12 18:05:05 +0800

    见识了include的魅力，但是我们需要传include的html中的变量哦，其实再继承的时候，我们还是要传值的

--- djb/djb/views.py
+++ djb/djb/views.py
@@ -3,18 +3,25 @@ from django.template.loader import get_template
 from django.template import Context
 import datetime
 
-def hello(request):
-    return HttpResponse("Hello world")
+# def hello(request):
+#     return HttpResponse("Hello world")
 
 # def current_datetime(request):
 #     now = datetime.datetime.now()
 #     html = "<html><body>It is now %s.</body></html>" % now
 #     return HttpResponse(html)
 
+def hello(request):
+    name = 'ysyong'
+    t = get_template('hello.html')
+    html = t.render(Context({'name': name}))
+    return HttpResponse(html)
+
 def current_datetime(request):
-    now = datetime.datetime.now()
+    name = 'ysyong'
+    current_date = datetime.datetime.now()
     t = get_template('current_datetime.html')
-    html = t.render(Context({'current_date': now}))
+    html = t.render(Context(locals()))
     return HttpResponse(html)
 
 def hours_ahead(request, offset):
