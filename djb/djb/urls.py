c5506075d1a7845bf534a7a907c9e7703479b7ae
ysyong <1156546473@qq.com>
2013-05-12 13:33:48 +0800

    设置了相关的url

--- djb/djb/urls.py
+++ djb/djb/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+from djb.views import hello
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,4 +15,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^hello/$', hello),
 )
