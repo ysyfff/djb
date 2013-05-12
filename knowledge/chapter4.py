7905613e0970205e2b456dd726e74416b442b007
ysyong <1156546473@qq.com>
2013-05-12 16:38:18 +0800

    为什么用python manage.py shell?

--- knowledge/chapter4.py
+++ knowledge/chapter4.py
@@ -11,3 +11,10 @@ My name is Adrian.
 c = template.Context({'name': 'Fred'})
 print t.render(c)
 # My name is Fred.
+
+
+'''The manage.py shell command has one key difference:
+before starting the interpreter, it tells Django which settings file to use.
+Many parts of Django, including the template system, rely on your settings,
+and you won’t be able to use them unless the framework knows which settings to use.
+'''
