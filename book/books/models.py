c79c86296a3a684b31d6d7703355c6fc8eb846b2
ysyong <1156546473@qq.com>
2013-05-13 23:50:37 +0800

    可选值的处理blank和null，.gitignore终于真正的正常了,记住他和.git在同一个目录就对了

--- book/books/models.py
+++ book/books/models.py
@@ -18,7 +18,7 @@ class Publisher(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
-    email = models.EmailField()
+    email = models.EmailField(blank=True)
 
     def __unicode__(self):
         return u'%s %s' % (self.first_name, self.last_name)
@@ -31,7 +31,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
-    publication_date = models.DateField()
+    publication_date = models.DateField(blank=True, null=True)
 
     def __unicode__(self):
         return self.title
