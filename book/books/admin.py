ae2d83566e36908821ff55c05238325229d520cf
ysyong <1156546473@qq.com>
2013-05-14 17:21:13 +0800

    定制admin的编辑列表,raw_id_fields的魅力

--- book/books/admin.py
+++ book/books/admin.py
@@ -14,6 +14,7 @@ class BookAdmin(admin.ModelAdmin):
     ordering = ('-publication_date',)
 
     filter_horizontal = ('authors',)
+    raw_id_fields = ('publisher',)
 
 
 admin.site.register(Publisher)
