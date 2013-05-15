c170da713663759f5ad2f5c75d6dfc9c5c43cc15
ysyong <1156546473@qq.com>
2013-05-15 22:46:15 +0800

    views中的默认参数初始值的用法

--- knowledge/chapter8.py
+++ knowledge/chapter8.py
@@ -39,4 +39,32 @@ def foobar_view(request, template_name):
 #-----------------The use of extra keyword in urlconf--------------------------#
 
 
+# urls.py
+
+from django.conf.urls.defaults import *
+from mysite import views
+
+urlpatterns = patterns('',
+    (r'^blog/$', views.page),
+    (r'^blog/page(?P<num>\d+)/$', views.page),
+)
+
+# views.py
+
+def page(request, num='1'):
+    # Output the appropriate page of blog entries, according to num.
+    # ...
+
+'''
+Here, both URL patterns point to the same view – views.
+page – but the first pattern doesn’t capture anything from the URL. 
+If the first pattern matches, 
+the page() function will use its default argument for num, '1'. 
+If the second pattern matches, 
+page() will use whatever num value was captured by the regular expression.
+'''
+#-----------------The use of default views arguments------------------------#
+
+
+
 
