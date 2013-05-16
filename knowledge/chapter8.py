3269c00d83353753543008716cb12e59f5fa3f43
ysyong <1156546473@qq.com>
2013-05-16 11:55:55 +0800

    增加了chapter9.py文件

--- knowledge/chapter8.py
+++ knowledge/chapter8.py
@@ -182,17 +182,3 @@ wrap them with requires_login in our URLconf:
 #----------------The use of requires_login(function)--------------------------#
 
 
-#---------------The use of context processors(request)-------------------------#
-django.core.context_processors.request
-If this processor is enabled, 
-every RequestContext will contain a variable request, 
-which is the current HttpRequest object. 
-Note that this processor is not enabled by default; 
-you have to activate it.
-
-You might want to use this 
-if you find your templates needing to access 
-attributes of the current HttpRequest such as the IP address:
-
-{{ request.REMOTE_ADDR }}
-#---------------The use of context processors(request)-------------------------#
