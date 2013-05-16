e897da124ecf074dd5054382e19af585ed5eec01
ysyong <1156546473@qq.com>
2013-05-16 16:45:08 +0800

    非常实用的model method

--- knowledge/chapter10.py
+++ knowledge/chapter10.py
@@ -80,3 +80,35 @@ class Person(models.Model):
 Person.men.all()
 Person.women.all()
 Person.people.all()
+
+
+Model methods:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+from django.contrib.localflavor.us.models import USStateField
+from django.db import models
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = USStateField() # Yes, this is U.S.-centric...
+
+    def baby_boomer_status(self):
+        "Returns the person's baby-boomer status."
+        import datetime
+        if datetime.date(1945, 8, 1) <= self.birth_date <= datetime.date(1964, 12, 31):
+            return "Baby boomer"
+        if self.birth_date < datetime.date(1945, 8, 1):
+            return "Pre-boomer"
+        return "Post-boomer"
+
+    def is_midwestern(self):
+        "Returns True if this person is from the Midwest."
+        return self.state in ('IL', 'WI', 'MI', 'IN', 'OH', 'IA', 'MO')
+
+    def _get_full_name(self):
+        "Returns the person's full name."
+        return u'%s %s' % (self.first_name, self.last_name)
+    full_name = property(_get_full_name)
+
