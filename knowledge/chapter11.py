5f54f4622dda6b70e6efdeba9aad271429ec10a6
ysyong <1156546473@qq.com>
2013-05-16 21:02:50 +0800

    通用视图在models中的用法

--- knowledge/chapter11.py
+++ knowledge/chapter11.py
@@ -19,6 +19,8 @@ the direct_to_template view simply grabs information
 from the extra-parameters dictionary and uses 
 that information when rendering the view.
 
+
+'''
 from django.http import Http404
 from django.template import TemplateDoesNotExist
 from django.views.generic.simple import direct_to_template
@@ -28,4 +30,40 @@ def about_pages(request, page):
         return direct_to_template(request, template="about/%s.html" % page)
     except TemplateDoesNotExist:
         raise Http404()
+
+
+Generic Views of Objects::::::::::::::::::::::::::::::::::::::::::::::::::
+from django.conf.urls.defaults import *
+from django.views.generic import list_detail
+from mysite.books.models import Publisher
+
+publisher_info = {
+    'queryset': Publisher.objects.all(),
+    'template_name': 'publisher_list_page.html',
+}
+
+urlpatterns = patterns('',
+    (r'^publishers/$', list_detail.object_list, publisher_info)
+)
+
 '''
+In the absence of template_name, though, 
+the object_list generic view will infer one from the object’s name. 
+In this case, 
+the inferred template will be 
+"books/publisher_list.html" – the “books” part comes from the name of 
+the app that defines the model, 
+while the “publisher” bit is just the lowercased version of the model’s name.
+'''
+
+{% extends "base.html" %}
+
+{% block content %}
+    <h2>Publishers</h2>
+    <ul>
+        {% for publisher in object_list %}
+            <li>{{ publisher.name }}</li>
+        {% endfor %}
+    </ul>
+{% endblock %}
+
