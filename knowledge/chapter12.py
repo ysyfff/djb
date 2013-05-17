21ea005e0dd49d8fb96e548ce412d66797d9dbb8
ysyong <1156546473@qq.com>
2013-05-17 14:14:50 +0800

    Deploy: TEMPLATE_DEBUG=FALSE

--- knowledge/chapter12.py
+++ knowledge/chapter12.py
@@ -4,6 +4,10 @@ Deploy Django:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 Turning Off Debug Mode:::::::::::::::::::::::::::::::
 set DEBUG to False.
 
+Turning Off Template Debug Mode::::::::::::::::::::::
+Similarly, you should set TEMPLATE_DEBUG to False in production.
+
+
 
 
 
